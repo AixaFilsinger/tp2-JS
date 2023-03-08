f3bb6f0c0a7d3b4619a82d77dcd8a5b6008d194f
Aixa Filsinger <macafil99@gmail.com>
2023-03-08 03:04:58 -0300

    Se realizo el ejercicio11

---
+++ js/ejercicio11.js
@@ -0,0 +1,29 @@
+
+
+
+let nombre1 = prompt("Por fa vor  ingrese su Nombre");
+let edad1 = parseInt(prompt("Por fa vor  ingrese su edad 1"));
+let nombre2 = prompt("Por fa vor  ingrese su Nombre");
+let edad2 = parseInt(prompt("Por fa vor  ingrese su edad 2"));
+let nombre3 = prompt("Por fa vor  ingrese su Nombre");
+let edad3 = parseInt(prompt("Por fa vor  ingrese su edad 3"));
+
+
+
+
+    let max = Math.max(edad1, edad2, edad3)
+
+    if(max == edad1){
+        document.write("El mayor es: " + nombre1);
+
+    }
+    if(max == edad2){
+        document.write("El mayor es: " + nombre2);
+
+    }
+    if(max == edad3){
+        document.write("El mayor es: " + nombre3);
+
+    }
+
+
